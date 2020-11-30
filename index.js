514df3ee8e5772e873c0de299f8259ab0f7af10c
Hazieon <handrew3721@gmail.com>
2020-11-30 22:22:01 +0000

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-
+const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
@@ -25,8 +25,8 @@ io.on("connection", (socket) => {
   // });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(PORT, () => {
+  console.log("listening on PORT");
 });
 
 // app.get("/", (req, res) => {
